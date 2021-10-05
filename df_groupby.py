5cb7cdb1d256ea5512210bdfd7de069e0e939dfd
AlTsao <al.tsao.yw@gmail.com>
2021-10-06 07:39:25 +0800

    revise

--- df_groupby.py
+++ df_groupby.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 #Read Data
 df = pd.DataFrame({'A':['X','X','Y','Y','Y','Y','Z','Z','Z','Z','Z','Z'],
-                   'B':['one','one','two','two','two','two','three','three','three','three','three','three'],
+                   'B':['one','one','two','two','three','three','four','four','five','five','six','six'],
                    'C':[1,2]*6,
                    'D':[3,4]*6})
 
@@ -14,12 +14,20 @@ print('============================================')
 
 
 #groupby - single group single column
-sglCol = df.groupby(['A'])['C'].sum() #將A分組，並且對於C做加總，但不會有C的title出現
+sglCol = df.groupby(['A'])['C'].sum() #以A作為分組標準，並且對於C做加總，但不會有C的title出現
 print('sglCol', sglCol.reset_index(), sep='\n')
 print('============================================')
 
 #groupby - multiple columns
-multiCol = df.groupby(['A'])[['C','D']].sum() #將A分組，並且對於C和D個別做加總，且有C和D的title出現
+multiCol = df.groupby(['A'])[['C','D']].sum() #以A作為分組標準，並且對於C和D個別做加總，且有C和D的title出現
 print('multiCol', multiCol.reset_index(), sep='\n')
+print('============================================')
 
-#groupby - multiple columns
+#groupby - multiple groups
+multiGroup = df.groupby(['A','B'])['C'].sum() #以A、B作為分組條件，對C做加總
+print('multiGroup', multiGroup.reset_index(), sep='\n')
+print('============================================')
+
+#multiple functions
+multiFunc = df.groupby(['A'])['C'].agg(['mean','sum']) #以A作為分組標準，對C進行平均與加總
+print('multiFunc', multiFunc.reset_index(), sep='\n')
