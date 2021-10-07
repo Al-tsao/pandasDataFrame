c0fd50f6bd13cc34601bd9ade456b4a699e8277c
AlTsao <al.tsao.yw@gmail.com>
2021-10-08 07:47:00 +0800

    revise

---
+++ df_allNumInCol.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({'col' : [1,2, 10, np.nan, 10], 
+                   'col2': ['a', 10, 30, 40 ,50],
+                   'col3': [1,2,3,4,5.0]})
+
+print(pd.to_numeric(df['col'], errors='coerce').notnull().all())
