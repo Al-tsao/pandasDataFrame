5585b6827b9637a15577cd6e8d9f235984af3b9e
AlTsao <al.tsao.yw@gmail.com>
2021-10-16 10:20:31 +0800

    revise

--- filter.py
+++ filter.py
@@ -1,11 +1,24 @@
 #載入模組
 import pandas as pd
 
-#載入數據
-df = pd.DataFrame([['Jhon',15,'A'],['Anna',19,'B'],['Paul',25,'D']])
-df. columns = ['Name','Age','Grade']
+#載入資料
+RawData = { "Day":['9/4', '9/5', '9/6', '9/7', '9/8', '9/9', '9/10', '9/11'],
+            "Revenue":[100, 120, 130, 110, 200, 160, 100, 180],
+            "Cost":[10.0, 10.0, 13.0, 11.0, 20.0, 16.0, 10.0, 18.0]}
 
-print(df)
+#轉化成DataFrame
+df = pd.DataFrame(RawData)
 
-#選到特定的index
-i = df[((df.Name == 'Jhon') & ( df.Age == 15) & (df.Grade == 'A'))].index.tolist()
+
+#使用Boolean-選到特定的index
+i = df[((df['Day'] == '9/11') & ( df['Revenue'] == 180) & (df['Cost'] == 18.0))].index.tolist()
+print(i)
+
+#使用isin
+mask_isin = df['Day'].isin(['9/4', '9/11'])
+
+#使用between
+mask_bet = df['Revenue'].between(160, 200)
+
+#使用dublicated
+mask_dupl = df['Cost'].duplicated()
