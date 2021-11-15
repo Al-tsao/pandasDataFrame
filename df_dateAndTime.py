c409e05d8c8d918894a16ccd79a72f3dbb08adda
unknown <al.tsao.yw@gmail.com>
2021-11-15 11:31:09 +0800

    revise

--- df_dateAndTime.py
+++ df_dateAndTime.py
@@ -6,10 +6,12 @@ from pandas.core.indexes.period import period_range
 
 #pd.to_datetime: 把資料轉換成pandas的時間格式
 dates = pd.DataFrame({'time': ['2021/01/01', '2021/12/31', 'Atome']})
-dates['timeFormant']  = pd.to_datetime(dates['time'], errors = 'coerce') 
-#errors: 如果沒有設定成coerce則在array中遇到錯誤的時間格式就會抱錯，若使用coerce就會把資料轉乘NaT
+dates['timeFormant']  = pd.to_datetime(dates['time'], errors = 'coerce', format = '%Y/%m/%d') 
+#errors: 如果沒有設定成coerce則在array中遇到錯誤的時間格式就會報錯，若使用coerce就會把資料轉乘NaT
+#format: pandas可以自己判定時間格式，但是有些時間格式太詭異這時候可以用format告訴pandas格式是如何
 
 #pd.data_range: 建立一段時間序列
+#利用區間建立時間序列
 dataRangeD = pd.date_range(start = '2021/01/01', end = '2021/12/31', freq = '1D') #freq = '1D': 間格是以1天為單位
 dataRangeB = pd.date_range(start = '2021/01/01', end = '2021/12/31', freq = 'B') #freq = 'B': 只取禮拜一到禮拜五
 dataRangeW = pd.date_range(start = '2021/01/01', end = '2021/12/31', freq = 'W-MON') #freq = 'W-MON': 只取禮拜一
@@ -17,10 +19,12 @@ dataRangeH = pd.date_range(start = '2021/01/01', end = '2021/12/31', freq = '1H'
 dataRangeM = pd.date_range(start = '2021/01/01', end = '2021/12/31', freq = 'M') #freq = 'M': 只取每個月的最後一天
 dataRangeMS = pd.date_range(start = '2021/01/01', end = '2021/12/31', freq = 'MS') #freq = 'MS': 只取每個月的第一天
 dataRangeA = pd.date_range(start = '2021/01/01', end = '2030/01/01', freq = 'A') #freq = 'A': 只取每一年的最後一天
+#利用指定數量建立時間序列-從頭往後推
+dataRangePeridStartD = pd.date_range(start = '2012/09/4', periods = 8, freq = '1D') # period: 要取多少的數量
+#利用指定數量建立時間序列-從尾往前推
+dataRangePeridEndD = pd.date_range(end = '2012/09/11', periods = 8, freq = '1D') # period: 要取多少的數量
 
-dataRangePeridD = pd.date_range(start = '2012/09/09', periods = 3, freq = '1D') # period: 要取多少的數量
 
 
 
-
-print(dataRangePeridD)
+print(dates['timeFormant'])
