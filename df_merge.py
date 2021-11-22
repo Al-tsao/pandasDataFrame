b7f0e3e119e4dd7355edcb72dad7db4512bf8f30
unknown <al.tsao.yw@gmail.com>
2021-11-22 14:58:05 +0800

    revise

--- df_merge.py
+++ df_merge.py
@@ -10,5 +10,8 @@ foods = pd.read_csv('Restaurant - Foods.csv')
 #pd.concat 直接將兩張表連結在一起
 sales = pd.concat(objs = [week1, week2], ignore_index = True) #ignore_index = True: 捨棄原本的index讓index重新自動生成，不然0~249的index會重複兩次
 sales = pd.concat(objs = [week1, week2], keys = ['Week 1', 'Week 2']) #keys: 建立雙重index
-print(sales.loc[('Week 1', 240), ['Customer ID', 'Food ID']]) #使用loc來找到雙重index的目標
+sales.loc[('Week 1', 240), ['Customer ID', 'Food ID']] #使用loc來找到雙重index的目標
+
+#merge
+print(week1.merge(week2, how = 'inner', on = 'Customer ID', suffixes = ['-Week 1', '-Week 2'])) #how = inner: 用交集取
 
