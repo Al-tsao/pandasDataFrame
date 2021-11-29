66b60aa9944e0128f1b97cad1c29f702cfd22976
AlTsao <al.tsao.yw@gmail.com>
2021-11-29 08:06:05 +0800

    revise

--- df_merge.py
+++ df_merge.py
@@ -6,6 +6,7 @@ week1 = pd.read_csv('Restaurant - Week 1 Sales.csv')
 week2 = pd.read_csv('Restaurant - Week 2 Sales.csv')
 customers = pd.read_csv('Restaurant - Customers.csv')
 foods = pd.read_csv('Restaurant - Foods.csv')
+satisfaction = pd.read_csv('Restaurant - Week 1 Satisfaction.csv')
 
 #pd.concat: 直接將兩張表連結在一起
 sales = pd.concat(objs = [week1, week2], ignore_index = True) #ignore_index = True: 捨棄原本的index讓index重新自動生成，不然0~249的index會重複兩次
@@ -33,5 +34,9 @@ merged = week2.merge(customers, how = 'left', left_on = 'Customer ID', right_on 
 customersIndex = pd.read_csv('Restaurant - Customers.csv', index_col = 'ID')
 foodsIndex = pd.read_csv('Restaurant - Foods.csv', index_col = 'Food ID')
 merged = week1.merge(customers, how = 'left', left_on = 'Customer ID', right_index = True)
+
+#join: 如果有想同的index可以用join不用merge的left join，可以簡化syntax
+merged = week1.join(satisfaction)
+
 
 print(merged)
