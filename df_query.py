fc80e57fbe85dd318e327746df38b9f30104e464
AlTsao <al.tsao.yw@gmail.com>
2021-12-06 10:56:06 +0800

    revise

---
+++ df_query.py
@@ -0,0 +1,17 @@
+"""
+效果跟filter一樣，雖然簡潔但是限制比較多，column名稱不能有空格，裡面要篩選的文字內容還要再用'或者"包起來
+"""
+
+#載入模組
+import numpy as np
+import pandas as pd
+
+#載入資料
+df = pd.DataFrame(np.random.randn(100, 3), columns = ['A', 'B', 'C'])
+df = df.stack().reset_index().iloc[:, ::-1]
+df.columns = ['Value', 'Column_Name', 'Index']
+#使用query
+df1 =df.query("Column_Name == 'A'")
+#使用filter
+mask = df['Column_Name'] == 'A'
+df2 = df[mask]
