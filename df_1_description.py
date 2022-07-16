3a1040916c5d4df8c49b30989ab1c0fa9aeea9ed
Al-tsao <al.tsao.yw@gmail.com>
2022-07-16 16:46:18 +0200

    revise

--- df_1_description.py
+++ df_1_description.py
@@ -3,53 +3,33 @@ import pandas as pd
 houses = pd.read_csv('kc_house_data.csv')
 houses
 
-# .head() 顯示資料前面幾筆
-print(' ')
-print('***********head***********')
-print('head', houses.head(10)) #數字是要看顯示幾筆資料
+#1 .head() 顯示資料前面幾筆
+houses.head(10) #數字是要看顯示幾筆資料
 
-# .tail()
-print(' ')
-print('***********tail***********')
-print(houses.tail(10)) #數字是要看顯示幾筆資料
+#2 .tail()
+houses.tail(10)#數字是要看顯示幾筆資料
 
-# .columns 返回所有欄位的名稱
-print(' ')
-print('***********columns***********')
-print(houses.columns)
+#3 .columns 返回所有欄位的名稱
+houses.columns
 
-# .shape 顯示資料大小，共幾行幾列
-print(' ')
-print('***********shape***********')
-print(houses.shape)
+#4 .shape 顯示資料大小，共幾行幾列
+houses.shape
 
-# .size 看有多少筆資料，就是column x row
-print(' ')
-print('***********size***********')
-print(houses.size)
+#5 .size 看有多少筆資料，就是column x row
+houses.size
 
-# .info() 顯示檔案詳細資訊，包含有多少column、有多少Row的資料(不包含nan)、column資料型態
+#6 .info() 顯示檔案詳細資訊，包含有多少column、有多少Row的資料(不包含nan)、column資料型態
 # 其中object是有文字和檔案大小
-print(' ')
-print('***********info***********')
-print(houses.info())
+houses.info()
 
-# .describe() 顯示相關敘述統計*
-    # 顯示有數據資料相關敘述統計
-print(' ')
-print('***********describe***********')
-print(houses.describe())
-    #顯示有文字資料相關敘述統計
-print(' ')
-print('***********describe***********')
-print(houses.describe(include = 'object'))
+#7 .describe() 顯示相關敘述統計
+#7.1 顯示有數據資料相關敘述統計
+houses.describe()
+#7.2 顯示有文字資料相關敘述統計
+houses.describe(include = 'object')
 
-# .unique() 顯示column中到底有多少個不同的值
-print(' ')
-print('***********unique***********')
-print(houses['date'].unique())
+#8 .unique() 顯示column中到底有多少個不同的值
+houses['date'].unique()
 
-# .dtypes 顯示每一個column的型態
-print(' ')
-print('***********dtypes***********')
-print(houses.dtypes)
+#9 .dtypes 顯示每一個column的型態
+houses.dtypes
--- df_3_read.py
+++ df_3_read.py
@@ -1,7 +1,28 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('kc_house_data.csv')
-
 data = pd.DataFrame(np.arange(16).reshape(4,4), index = list('abcd'), columns = list('ABCD'))
-print(data)
+
+# 提取DataFrame中特定位置
+#1 取索引為'a'的row
+data.loc['a']
+data.iloc[0]
+
+#2 取A和D的column
+data.loc[:,['A', 'D']]
+data.iloc[:,[0, 3]]
+
+#3 取特定index和column
+data.loc[['a','b'],['A','B']]
+data.iloc[[0,1],[0,1]]
+
+#4 取所有值
+data.loc[:,:]
+data.iloc[:,:]
+
+#5 跳著選
+data.iloc[:,::2] 
+#start:stop:step ref:https://stackoverflow.com/questions/509211/understanding-slice-notation
+
+#6 反過來選
+data.iloc[:,::-1] #start:stop:step
