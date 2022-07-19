c73056fcbdc5110c40def5a0f03c06a71cda366d
Al-tsao <al.tsao.yw@gmail.com>
2022-07-19 05:33:30 +0200

    revise

---
+++ df_8_filter.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+RawData = { "Day":['9/4', '9/5', '9/6', '9/7', '9/8', '9/9', '9/10', '9/11'],
+            "Revenue":[100, 120, 130, 110, 200, 160, 100, 180],
+            "Cost":[10.0, 10.0, 13.0, 11.0, 20.0, 16.0, 10.0, 18.0]}
+df = pd.DataFrame(RawData)
+
+#1 使用Boolean篩選
+#1.1 單一條件
+mask = df['Day']=='9/4'
