e36f56255a96c748db6dcf459466ccb56f09f8b4
Al-tsao <al.tsao.yw@gmail.com>
2022-07-22 14:58:13 +0200

    revise

--- df_13_groupBin.py
+++ df_13_groupBin.py
@@ -1,10 +1,14 @@
+"""
+利用欄位裡的代號重新給上新的標籤，像是原本欄位裡面有1和2，可以利用1和2新增加一個欄位，生成男或女
+"""
+
 #載入模組
 import pandas as pd
 
 #Read Data
 df = pd.DataFrame({'A':['X','X','Y','Y','Y','Y','Z','Z','Z','Z','Z','Z'],
                    'B':['one','one','two','two','three','three','four','four','five','five','six','six'],
-                   'C':[1,2]*6,
+                   'C':[1,2,3,2,1,1,1,1,1,1,1,1],
                    'D':[3,4]*6})
 
 #讀取column中的title
@@ -13,5 +17,5 @@ print('Title名稱', title, sep='\n')
 print('============================================')
 
 #分組取名
-df['bins'] = pd.cut(df['C'], bins=2, labels=('甲','乙'))
+df['bins'] = pd.cut(df['C'], bins=3, labels=('甲','乙','丙'))
 print(df)
