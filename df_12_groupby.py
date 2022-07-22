87f3df4967d26f926588a6602ee96290650fbd95
Al-tsao <al.tsao.yw@gmail.com>
2022-07-22 14:25:55 +0200

    revise

--- df_12_groupby.py
+++ df_12_groupby.py
@@ -1,7 +1,5 @@
-#載入模組
 import pandas as pd
 
-#Read Data
 df = pd.DataFrame({'A':['X','X','Y','Y','Y','Y','Z','Z','Z','Z','Z','Z'],
                    'B':['one','one','two','two','three','three','four','four','five','five','six','six'],
                    'C':[1,2]*6,
@@ -12,35 +10,38 @@ title = pd.DataFrame({'Title': df.columns})
 print('Title名稱', title, sep='\n')
 print('============================================')
 
+#1 groupby: 分組，將資料以'A'欄內資料的方式分組，然後放到dic中
+groupbyA = df.groupby('A')
+print('groupbyA', groupbyA.groups)
+print('============================================')
 
-#groupby - single group single column
+#2 groupby - single group single column
 sglCol = df.groupby(['A'])['C'].sum() #以A作為分組標準，並且對於C做加總，但不會有C的title出現
 print('sglCol', sglCol.reset_index(), sep='\n')
 print('============================================')
 
-#groupby - multiple columns
+#3 groupby - multiple columns
 multiCol = df.groupby(['A'])[['C','D']].sum() #以A作為分組標準，並且對於C和D個別做加總，且有C和D的title出現
 print('multiCol', multiCol.reset_index(), sep='\n')
 print('============================================')
 
-#groupby - multiple groups
+#4 groupby - multiple groups
 multiGroup = df.groupby(['A','B'])['C'].sum() #以A、B作為分組條件，對C做加總
 print('multiGroup', multiGroup.reset_index(), sep='\n')
 print('============================================')
 
-#multiple functions
+#5 multiple functions
 multiFunc = df.groupby(['A'])['C'].agg(['mean','sum']) #以A作為分組標準，對C進行平均與加總
 print('multiFunc', multiFunc.reset_index(), sep='\n')
 
-#size()
+#6 size()
 sectors = df.groupby('A')
 sectors.size() #其實跟value_counts()的功能一項，是了解一個series中各分類的總數是多少
 
-#get_group()
+#7 get_group()
 print(sectors.get_group('Z')) #只會抽取Sector中特定的Z組，將資料傳出來
 
-
-#agg()
+#8 agg()
 countList = [1]*df.shape[0]
 df['Count'] = countList #新增Count欄位來計算次數
 agg_d = {'C':'sum','Count':'size'} #定義column欄位要做甚麼統計運算
