0ed0f4212c6e66f5d1664207eb0b7f0c11064cdc
Al-tsao <al.tsao.yw@gmail.com>
2022-07-22 12:59:08 +0200

    revise

--- df_allNumInCol.py
+++ df_allNumInCol.py
@@ -1,10 +1,11 @@
+"""
+目的是要看單一個colum中是否nan的存在
+"""
+
 import numpy as np
 import pandas as pd
 
 df = pd.DataFrame({'col' : [1,2, 10, np.nan, 10], 
                    'col2': ['a', 10, 30, 40 ,50],
                    'col3': [1,2,3,4,5.0]})
-
-print(pd.to_numeric(df['col'], errors='coerce').notnull())
-
-# print(pd.to_numeric(df['col'], errors='coerce').notnull().all())
+print(pd.to_numeric(df['col'], errors='coerce').notnull().all())
